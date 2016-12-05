79be2311851427654db1ece3d8e9c2d4397b6c29
NodeXperts16 <nodeexperts@gmail.com>
2016-12-05 20:05:19 +0530

    Updated rank scrapers

--- scraper.js
+++ scraper.js
@@ -10,51 +10,43 @@ var bing = require('./lib/bing');
  * @param lang
  * @param start
  * @param proxy
+ * @param type
  * @param callback
  */
-function scraper(query, sedb, lang, start, proxy, callback) {
-    var scrapedResult = {};
-    var i = 0;
-    google(query, sedb, lang, start, proxy, function (err, next, links) {
-        i++;
-        if (err) {
-            callback(err);
-        } else {
-            scrapedResult.google = links;
-            if (i >= 3) {
-                callback(null, scrapedResult);
-            }
-        }
-    });
-    bing(query, sedb, lang, start, proxy, function (err, next, links) {
-        i++;
-        if (err) {
-            callback(err);
-        } else {
-            scrapedResult.bing = links;
-            if (i >= 3) {
-                callback(null, scrapedResult);
-            }
-        }
-    });
-    yahoo(query, sedb, lang, start, proxy, function (err, next, links) {
-        i++;
-        if (err) {
-            callback(err);
-        } else {
-            scrapedResult.yahoo = links;
-            if (i >= 3) {
-                callback(null, scrapedResult);
-            }
-        }
-    });
+function scraper(query, sedb, lang, start, proxy, type, callback) {
+   if(type === 'yahoo') {
+       yahoo(query, sedb, lang, start, proxy, function (err, next, links) {
+           i++;
+           if (err) {
+               callback(err);
+           } else {
+               callback(null, links);
+           }
+       });
+   } else if(type === 'bing') {
+       bing(query, sedb, lang, start, proxy, function (err, next, links) {
+           i++;
+           if (err) {
+               callback(err);
+           } else {
+               callback(null, links);
+           }
+       });
+   } else {
+       google(query, sedb, lang, start, proxy, function (err, next, links) {
+           i++;
+           if (err) {
+               callback(err);
+           } else {
+               callback(null, links);
+           }
+       });
+   }
 }
 module.exports = scraper;
 
 // In order to test, uncomment this part and run node scraper.js
 
-//scraper('weekly meal planner', 'us', 'en', null, 'ltpc170d53a:c170d53a@ltp.viia.me:8074', function (err, links) {
-//    console.log('google ', links.google.length)
-//    console.log('yahoo ',links.yahoo.length)
-//    console.log('bing ', links.bing.length)
+//scraper('weekly meal planner', 'us', 'en', null, 'ltpc170d53a:c170d53a@ltp.viia.me:8074', 'google', function (err, links) {
+//    console.log('google ', links)
 //});
